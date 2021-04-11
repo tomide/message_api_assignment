65921279d4d8f2196a1d3248ca3ff94df51f61ab
tomadedeji <ayotomideadedeji@gmail.com>
2021-04-11 16:52:53 -0400

    "done clean up on create message end point"

--- src/main/scala/com/qlik/map/message/api/MessageApiService.scala
+++ src/main/scala/com/qlik/map/message/api/MessageApiService.scala
@@ -1,34 +1,23 @@
 package com.qlik.map.message.api
 
-import cats.effect.ExitCase.complete
 import com.qlik.map.message.api.MessageApiUtil.{compressMessage, isWordPalindrome, isWordValid, md5Harsher}
 import com.qlik.map.message.messageApiService._
 import com.typesafe.scalalogging.StrictLogging
 import monix.eval.Task
-import org.http4s.MessageFailure
-import org.http4s.Status.AlreadyReported
 import org.mongodb.scala.model.Filters
 import org.mongodb.scala.model.Filters._
 import org.mongodb.scala.model.Projections._
 import org.mongodb.scala.result.{DeleteResult, UpdateResult}
-import org.mongodb.scala.{Completed, Document, MongoCollection, MongoException, Observer}
-
+import org.mongodb.scala.{Completed, Document, MongoCollection, Observer}
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scala.util.{Failure, Success}
 
 trait MessageApiService {
-
   def createMessage(userMessage: createRequest): Task[feedBack]
-
   def retrieveMessage(userMessage: retrieveRequest): Task[retrieveResponse]
-
   def updateMessage(userMessage: updateRequest): Task[feedBack]
-
   def deleteMessage(userMessage: deleteRequest): Task[feedBack]
-
   def listAllMessages(): Task[Seq[feedBack]]
-
 }
 
 object MessageApiService {
@@ -58,33 +47,24 @@ class MessageApiServiceIO(collection: MongoCollection[Document]) extends Message
   }
 
   override def retrieveMessage(req: retrieveRequest): Task[retrieveResponse] = {
-
     val returnedDocument = collection.find(equal("_id", md5Harsher(req.message)))
       .projection(fields(include("message", "is_word_palindrome"))).first()
-
     val listMapScores = returnedDocument.map(doc => (doc("message").asString.getValue,
       doc("is_word_palindrome").asBoolean().getValue)).collect().toFuture()
-
     Task.fromFuture(for {
       result <- listMapScores.map(contentSequence => {
         val contentTuple = contentSequence.head
         retrieveResponse(contentTuple._1, contentTuple._2)
       })
     } yield result)
-
   }
-
 
   override def deleteMessage(req: deleteRequest): Task[feedBack] = {
     val document = Document("_id" -> md5Harsher(req.message))
     val deleteObservable = collection.deleteMany(document)
     deleteObservable.subscribe(observer = new Observer[DeleteResult] {
-      override def onNext(result: DeleteResult): Unit = {
-        logger.info(s""" message - ${req.message} was deleted""")
-      }
-
+      override def onNext(result: DeleteResult): Unit = logger.info(s""" message - ${req.message} was deleted""")
       override def onError(e: Throwable): Unit = println(s"onError: $e")
-
       override def onComplete(): Unit = println("onComplete")
     })
     Task.now(feedBack(s"word: ${req.message} was deleted"))
@@ -96,9 +76,7 @@ class MessageApiServiceIO(collection: MongoCollection[Document]) extends Message
       .updateOne(Filters.eq("_id", md5Harsher(req.oldMessage)), updateDocument)
     updateObservable.subscribe(new Observer[UpdateResult] {
       override def onNext(result: UpdateResult): Unit = println(s"onNext: ${result.toString} was updated")
-
       override def onError(e: Throwable): Unit = println(s"onError: $e")
-
       override def onComplete(): Unit = println("onComplete")
     })
     Task.now(feedBack(s"old_message:- ${req.oldMessage} has been updated with new_message :- ${req.newMessage}"))
