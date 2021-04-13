96afa139444028ab8c74f948ce683f4134d2c559
Tom Adedeji <ayotomideadedeji@gmail.com>
2021-04-13 15:16:07 -0400

    Update MessageApiRoutes.scala
    update read med

--- src/main/scala/com/qlik/map/message/api/MessageApiRoutes.scala
+++ src/main/scala/com/qlik/map/message/api/MessageApiRoutes.scala
@@ -18,28 +18,50 @@ import org.mongodb.scala.{MongoBulkWriteException, MongoException}
 (NB: A valid message is a message that does contain any numerical values)
  */
 /**
- 1. create message route GET / :-
-this route accepts a post request in the following format [{ "message" : "<valid_message>" }]
-     a. if valid message is received, this enpoint should return :- [{ "response": "created_word: <your_message>, is_word_palindrome: <Boolean>" }]
-     b. if valid message is received and message already exist in database, this endpoint should return :- { "response": "message: '<your_message>' already exist, is_word_palindrome: <Boolean>" }
+ 1. create message route :  GET / :-
+this route accepts a post request 
+example:-  [{ "message" : "<valid_message_1>" }, { "message" : "<invalid_message_2>" }, , { "message" : "<a_duplicate_message>" }] // this s list of create requests
+response:- [{ "response": "_id: modified_md5_hash_used_in_mongodb_id, message: <valid_message_1> , status: created}
+     { "response": message: <invalid_message_2> , status: invalid message format },
+     {"response": "message: <a_duplicate_message> status: failed with error : <a_long_error_mongodb> }]}
  */
 /**
-  2. retrieve message route  => /  :-
-this route accepts a post request in the following format [{ "message" : "<valid_message>" }]
-     a. if existing message is received, this end point should return :- [{ "response": "message: <your_message>, is_word_palindrome: <Boolean>" }]
-     b. if non existing message is received, this endpoint should return :- [{ "message: <your_message> not in database" }]
+  2. retrieve message route:  POST => / list_message  :-
+this route accepts a post request: 
+example:- [{ "messageId" : "<valid_message_id>" }, { "messageId" : "<valid_message_id>" }, { "messageId" : "<nonExisting_message_id>" }] // list of messages to retrieve
+response :- [{ "response": "message : ayayass, is_word_palindrome : <Boolean>" },
+     { "response": "message : ayayass, is_word_palindrome : <Boolean>"  },
+     { "response": "message :<nonExisting_message_id>, no record found" }]
  */
 /**
-  3. delete message route => / <your_message_id> :-
-this route accepts a delete request, with a message at the end of the request api
-     a. if existing message is received, this end point should return :- { "response": "message: <your_message> was deleted" }
+  3. delete message route:  DELETE => / <your_message_id> :-
+this route accepts a delete request, with a messageId at the end of the request api
+     a. if an existing messageId is received, this end point should return :- { "response": "message: <your_message_id> was deleted" }
      b. if non existing message is received, this endpoint should return :- { "message": "No record found for message :- messageId : not Found" }
  */
 /**
-   4. update message route /  :-
-this route accepts a pur request in the following format [{ "old_message" : "<old_valid_message>" , "new_message" : "<new_valid_message>" }]
-     a. if existing message is received, this end point should return :- { "response": "old_message: <old_message> has been updated with new message :- <new_message_in_body>" }
-     b. if non existing message is received, this endpoint should return :- { "old_message: <old_your_message> was added to the database " }
+   4. update message route: PUT /  :-
+example :- [{
+    "oldMessage" : <old_message_1> , "newMessage" : <new_message_1>
+},
+{
+    "oldMessage" : <old_message_2> , "newMessage" : <new_message_2>
+},
+{
+    "oldMessage" : <non_existing_old_message> , "newMessage" : <new_message_2>
+}
+]
+reponse :- [
+    {
+        "response": "old_messages:- <old_message_1> as been updated with new_message <new_message_1>"
+    },
+    {
+        "response": "old_messages:- "<old_message_2> as been updated with new_message <new_message_1>"
+    },
+    {
+        "response": message: <non_existing_old_message> was added to the database "
+    }
+]
  */
 
 
@@ -68,7 +90,7 @@ object MessageApiRoutes extends StrictLogging {
           case _ => BadRequest(ResponseError("error_message : Bad request / Decoder - Encoder Failed").asJson)
         }
 
-      case req @ POST -> Root / "list_message" =>
+      case req @ POST -> Root / "list_message_id" =>
         (for {
           decodedReq <- req.as[Seq[retrieveRequest]]
           messageRequested <- messageService.retrieveMessage(decodedReq)
